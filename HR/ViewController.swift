a1a7e1b55ef5e83ce71b6998520acb177464517f
Karan Thukral <karanthukral@gmail.com>
2016-03-15 21:21:58 -0400

    Initial Commit

---
+++ HR/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HR
+//
+//  Created by Karan Thukral on 2016-03-15.
+//  Copyright © 2016 Karan Thukral. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
